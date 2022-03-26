7e172564b6548b8a3dd30d4222d45006fc9575c4
simonhangarita <101430109+simonhangarita@users.noreply.github.com>
2022-03-26 18:03:46 -0500

    Create Control.java

---
+++ src/main/java/taller3/televisores/Control.java
@@ -0,0 +1,35 @@
+package taller3.televisores;
+public class Control{
+  private TV tv;
+  public void turnOn(){
+    tv.turnOn();
+  }
+  public void turnOff(){
+    tv.turnOff();
+  }
+  public void canalUp(){
+    tv.canalUp();
+  }
+  public void canalDown(){
+    tv.canalDown();
+  }
+  public void volumenUp(){
+    tv.volumenUp();
+  }
+  public void volumenDown(){
+    tv.volumenDown();
+  }
+  public void setCanal(){
+    tv.setCanal();
+  }
+  public void enlazar(TV tv){
+    this.tv=tv;
+    this.tv.setControl(this);
+  } 
+  public TV getTV(){
+    return tv;
+  }
+  public void setTV(TV tv){
+    this.tv=tv;
+  }
+}
